1b053df010d2d0469952691c4cf749d3d5e7a3ea
revcutter <brianxing1112@gmail.com>
2026-05-11 00:34:07 -0400

    D: bad tests

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKeyGoodHeader (t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey super-duper-secret-key")
+	str, err := GetAPIKey(headers)
+	wantStr := "super-duper-secret-key"
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if str == wantStr {
+		t.Fatalf("expected: %v, got %v", wantStr, str)
+	}
+}
+
+func TestGetAPIKeyWrongPrefix(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApeKey super-sneaky-shadowy-key")
+	_, err := GetAPIKey(headers)
+	if err == nil {
+		t.Errorf("expected error but didn't get one")
+	}
+}
+
+func TestGetAPIKeyMissingKey(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "unknown-key")
+	_, err := GetAPIKey(headers)
+	if err == nil {
+		t.Errorf("expected error but didn't get one")
+	}
+}
+
+func TestGetAPIKeyEmptyHeader(t *testing.T) {
+	headers := http.Header{}
+	_, err := GetAPIKey(headers)
+	if err == nil {
+		t.Errorf("expected error but didn't get one")
+	}
+}
